c3ae11e3e2ddae6fa42384f4a15888edd966ac84
Helal Salloum <helalsalloum77@gmail.com>
2026-03-08 03:31:31 +0300

    feat(auth): add bcrypt password hashing and typed DTOs in service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable , BadRequestException } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
+import { SignupDto } from "./dto/signup.dto";
+import { LoginDto } from "./dto/login.dto";
+
 
 @Injectable()
 export class AuthService {
     constructor(private prisma : PrismaService) {}
 
-    async signup(dto : any) {
+    async signup(dto : SignupDto) {
         const userExists = await this.prisma.user.findUnique({
             where: { email : dto.email }
         })
@@ -23,7 +26,7 @@ export class AuthService {
         return {message : "User created" , user:{email:newUser.email}}
     }
 
-    async login(dto : any) {
+    async login(dto : LoginDto) {
         const user = await this.prisma.user.findUnique({
             where : {email : dto.email}
         })
