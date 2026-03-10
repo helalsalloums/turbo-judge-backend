ae90004766b5ffeb5ddb8521290817ab1064dac7
Helal <helalsalloum77@gmail.com>
2026-03-10 21:43:21 +0300

    feat : connect SubmissionModule to PostgreSQL via PrismaModule

--- src/submission/submission.service.ts
+++ src/submission/submission.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSubmissionDto } from './dto/create-submission.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SubmissionService {
-  create(createSubmissionDto: CreateSubmissionDto) {
-    return { status: "accepted", time: 42 };
+  constructor(private prisma: PrismaService) { }
+
+  async create(createSubmissionDto: CreateSubmissionDto) {
+    const newSubmission = await this.prisma.submission.create({
+      data: {
+        code: createSubmissionDto.code,
+        language: createSubmissionDto.language,
+        problemId: createSubmissionDto.problemId,
+      }
+    })
+
+    return { message: "Submission created", submission: newSubmission }
   }
 
 }
