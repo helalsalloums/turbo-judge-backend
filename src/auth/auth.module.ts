635da59d44519d292c6d5265869fd6b78b0ec0f3
Helal <helalsalloum77@gmail.com>
2026-03-17 02:39:43 +0300

    feat(auth) : add JwtStrategy to auth module providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
