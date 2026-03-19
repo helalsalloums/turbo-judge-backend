7b9083666f0466c3b388cd29e20172bf32735a32
Helal <helalsalloum77@gmail.com>
2026-03-19 14:31:59 +0300

    feat(judge): implement judge() with compile, run, and verdict logic

--- src/submission/judge.service.ts
+++ src/submission/judge.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { spawn } from "child_process";
+import { randomUUID } from "crypto";
+import { writeFile } from "fs/promises";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class JudgeService {
+
+  constructor(private prisma: PrismaService) { }
+
   private async compile(sourceFile: string, outputBinary: string): Promise<void> {
     return new Promise((resolve, reject) => {
 
@@ -41,5 +47,36 @@ export class JudgeService {
     });
   }
 
-  async judge(submissionId: number): Promise<string> { }
+  async judge(submissionId: number): Promise<string> {
+
+    const submission = await this.prisma.submission.findUnique({
+      where: { id: submissionId }
+    })
+
+    const problem = await this.prisma.problem.findUnique({
+      where: { id: submission!.problemId }
+    })
+
+    const testCases = await this.prisma.testCase.findMany({
+      where: { problemId: submission!.problemId }
+    });
+
+    if (!submission) throw new Error('Submission not found');
+    if (!problem) throw new Error('Problem not found');
+    if (!testCases) throw new Error('TestCases not found');
+
+    const sourceFile = `/tmp/${randomUUID()}.cpp`;
+    await writeFile(sourceFile, submission!.code);
+    const binaryFile = `/tmp/${randomUUID()}`;
+    await this.compile(sourceFile, binaryFile);
+
+    for (let i = 0; i < testCases.length; i++) {
+      const output = await this.run(binaryFile, testCases[i].input, problem!.timeLimit);
+      if (output !== testCases[i].output) {
+        throw (new Error(`Wrong answer on test ${i + 1}`));
+      }
+    }
+
+    return 'Accepted';
+  }
 }
